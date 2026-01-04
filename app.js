de848dc80440ce3b91bf5a35144ac4cb74abbb11
khadijacode123-hue <khadija.code123@gmail.com>
2026-01-05 00:51:33 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// =========>Question:1<=========
+
+var empty = [];
+// =========>Question:2<=========
+var students = ["", "", "", "", "", "", "", "", "", ""];
+
+// =========>Question:3<=========
+var students = ["Khadija", "Ayesha", "Sana", "Amna", "Zainab"];
+
+// =========>Question:4<=========
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// =========>Question:5<=========
+
+var answers = [true, false, false, true];
+
+// =========>Question:6<=========
+
+var mixed = ["Arwa", 19, true, false, "Ali", 20];
+
+// =========>Question:7<=========
+
+var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil.", "PhD"];
+document.write("<h1>Qualifications:</h1> <br>");
+document.write("1)" + qualifications[0] + "<br>")
+document.write("2)" + qualifications[1] + "<br>")
+document.write("3)" + qualifications[2] + "<br>")
+document.write("4)" + qualifications[3] + "<br>")
+document.write("5)" + qualifications[4] + "<br>")
+document.write("6)" + qualifications[5] + "<br>")
+document.write("7)" + qualifications[6] + "<br>")
+document.write("8)" + qualifications[7] + "<br>")
+
+// =========>Question:8<=========
+
+var students = ["Khadija", "Ayesha", "Sana"];
+var score = [300, 450, 350];
+var totalMarks = 500;
+var percentage = (score / totalMarks * 100);
+document.write("Score of" + students[0] + "is" + score[0] + "Percantage:" + score[0] / totalMarks * 100 + "%<br>");
+document.write("Score of" + students[1] + "is" + score[1] + "Percantage:" + score[1] / totalMarks * 100 + "%<br>")
+document.write("Score of" + students[2] + "is" + score[2] + "Percantage:" + score[2] / totalMarks * 100 + "%<br>")
+
+// =========>Question:9<=========
+
